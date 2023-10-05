40d0b34bc090e08555ef128cafedff0b78475547
William Parsons <64643504+W8pars@users.noreply.github.com>
2023-10-05 10:34:35 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,21 @@
-
-
-<?php require 'master.php';?>
+<?php
+include 'master.php';
+?>
 
 <div class="container text-center">
 	<h1> Welcome to the Login Page </h1>
 </div>
 <div class="container text-center">
-	<form>
+	<form action="includes/login.inc.php" method="POST">
 		<div class="form-group">
-    			<label for="userEmailInput">Email address</label>
-   			<input type="email" class="form-control" id="userEmailInput" aria-describedby="emailHelp" placeholder="Enter email">
-    			<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    			<label for="email">Email address</label>
+   			<input type="email" name="email" class="form-control" id="email" placeholder="Enter email">
   		</div>
   		<div class="form-group">
-    			<label for="userPasswordInput">Password</label>
-    			<input type="password" class="form-control" id="userPasswordInput" placeholder="Password">
+    			<label for="password">Password</label>
+    			<input type="password" name="password" class="form-control" id="password" placeholder="Password">
   		</div>
-  		<button type="submit" class="btn btn-primary">Submit</button>
+  		<button type="submit" name="submit" class="btn btn-primary">Login</button>
 	</form>
 
-<?php require 'footer.php';?>
+<?php include 'footer.php';?>
